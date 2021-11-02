e6734977443c285b6e737ce5034b39a3d9f1fd39
lukaaas <lukas-graf@hotmail.com>
2021-11-02 15:41:19 +0100

    Push von Lukas

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld Anne");
         System.out.println("Hallo world Sihem");
+        System.out.println("Hallo Welt Lukas");
 
     }
 
