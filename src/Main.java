f931c43186bede6daf0e659f8f19977da03bbd76
som-ould <56541608+som-ould@users.noreply.github.com>
2021-11-02 15:39:55 +0100

    Sam

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("HelloWorld Anne ");
+        System.out.println("HelloWorld");
+        System.out.println("Hallo world Sihem");
+
     }
 
 }
