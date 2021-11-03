79be58db68ed9aa361e1e8494b8b332e4ca62d33
som-ould <56541608+som-ould@users.noreply.github.com>
2021-11-03 11:34:50 +0100

    Connection

--- src/Sales.java
+++ src/Sales.java
@@ -1,9 +1,10 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Sales
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("","dbi","");
     }
 }
