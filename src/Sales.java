91957e4c39c8b6d42c0737b6b3791943a4ecf302
som-ould <56541608+som-ould@users.noreply.github.com>
2021-11-09 14:21:00 +0100

    Connection

--- src/Sales.java
+++ src/Sales.java
@@ -1,10 +1,17 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Sales
 {
     public static void main(String[] args) throws SQLException {
-        Connection conn = DriverManager.getConnection("","dbi","");
+        String pid = "p01";
+        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:10","sam","password");
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select o.aid, SUM(o.qty * o.dollars) as sales from dbi.orders as o where o.pid = '"+pid+"' group by o.aid");
+
+        while (rs.next()) {
+            String aid = rs.getString("aid");
+            String sales = rs.getString("sales");
+            System.out.println(aid +": "+sales);
+        }
     }
 }
