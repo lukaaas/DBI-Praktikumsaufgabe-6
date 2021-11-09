735adddc3cc8f639a9cd99c8394d929a322fc3f8
lukaaas <lukas-graf@hotmail.com>
2021-11-09 14:48:33 +0100

    Push von Lukas

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.sql.*;
+
+//10.0.2.15
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("HelloWorld");
-    }
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.178.47:3306/DBI", "dbi","root");
 
+            System.out.println("Connect");
+        }catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+    }
 }
+
